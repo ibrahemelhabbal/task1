ea5b6a119b95d2d4bff77ac5b53d89eb1b4f90eb
ibrahem elhabbal <160194836+ibrahemelhabbal@users.noreply.github.com>
2024-09-18 08:20:08 +0300

    Add files via upload

---
+++ task1/script.js
@@ -0,0 +1,16 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const hamburger = document.querySelector('.hamburger');
+    const navMenu = document.querySelector('.nav-menu');
+
+    hamburger.addEventListener('click', () => {
+        hamburger.classList.toggle('active');
+        navMenu.classList.toggle('active');
+    });
+
+    document.querySelectorAll('.nav-menu li a').forEach(link => {
+        link.addEventListener('click', () => {
+            hamburger.classList.remove('active');
+            navMenu.classList.remove('active');
+        });
+    });
+});
